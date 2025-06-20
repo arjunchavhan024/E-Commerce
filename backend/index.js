e469eb51a78124e5614bff4dd69d682e9aecef51
Arjun Chavhan <ac4718770@gmail.com>
2025-06-20 11:24:13 +0530

    connected to db

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,19 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const productRoutes = require("./routes/productRoutes");
 
 const app = express();
+const URL = process.env.MONGO_URL;
+
 app.use(cors());
 app.use(express.json());
-app.use("/api/products", productRoutes);
 
 mongoose
-  .connect("mongodb://localhost:27017/geer", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    app.listen(5000, () =>
-      console.log("Server running on http://localhost:5000")
-    );
-  })
-  .catch(console.error);
+  .connect(URL)
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.error("DB connection error:", err));
+
+app.listen(5000, () => {
+  console.log("App is listening on port 5000");
+});
